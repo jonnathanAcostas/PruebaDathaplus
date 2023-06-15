3cc3cf6479815bb8a4e79991afce058a5c16a14d
jonnathanAcostas <jpc.acosta@yavirac.edu.ec>
2023-06-14 21:18:44 -0500

    correccion metodo get y create

--- src/phone/createPhone.js
+++ src/phone/createPhone.js
@@ -1,9 +1,48 @@
-const create = async (event) =>{
-//const {title} = event.body;
-console.log ("title", event.body);
-return {
-    statuscode: 200
-}
-}
+const { v4 } = require("uuid");
+const AWS = require("aws-sdk");
 
-module.exports = {create};
+
+const create = async (event) => {
+const postulantHeader = event.headers['postulant'];
+if (postulantHeader === "You're the best") {
+
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  const { title, description } = JSON.parse(event.body);
+  const createdAt = new Date();
+  const id = v4();
+
+  console.log("created id: ", id);
+
+  const newTask = {
+    id,
+    title,
+    description,
+    createdAt
+  };
+
+  await dynamodb
+    .put({
+      TableName: "PhoneTable",
+      Item: newTask,
+    })
+    .promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(newTask),
+  };
+}
+else{
+    // Propiedad personalizada o valor incorrectos, enviar una respuesta de error
+    return {
+        statusCode: 401,
+        body: JSON.stringify({ message: 'Unauthorizer' }),
+      };
+}
+};
+
+module.exports = {
+  create
+};
--- src/phone/getPhone.js
+++ src/phone/getPhone.js
@@ -1,9 +1,21 @@
-const get = async (event) =>{
-    //const {title} = event.body;
-    console.log ("title");
-    return {
-        statuscode: 200
-    }
-    }
-    
-    module.exports = {get};
+const AWS = require("aws-sdk");
+
+const get = async (event) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  const result = await dynamodb.scan({ TableName: "PhoneTable" }).promise();
+
+  const tasks = result.Items;
+console.log(tasks);
+
+  return {
+    status: 200,
+    body: {
+      tasks
+    },
+  };
+};
+
+module.exports = {
+  get,
+};
